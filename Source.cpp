3b1e04a508cea5092080a17f15ae9c9daf42a9ae
vladik <deez1dayz@gmail.com>
2026-04-18 10:46:23 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -130,6 +130,138 @@ private:
         return bestIndex;
     }
 };
+void printEntry(const BiDirectionalPriorityQueue::Entry& entry) {
+    cout << "Item: " << entry.item
+        << " | Priority: " << entry.priority
+        << " | Order: " << entry.order << endl;
+}
 
+int main() {
+    BiDirectionalPriorityQueue queue;
+    int choice;
+
+    do {
+        cout << "\n===== BI-DIRECTIONAL PRIORITY QUEUE =====\n";
+        cout << "1. Enqueue item\n";
+        cout << "2. Dequeue highest priority\n";
+        cout << "3. Dequeue lowest priority\n";
+        cout << "4. Dequeue oldest item\n";
+        cout << "5. Dequeue newest item\n";
+        cout << "6. Peek highest priority\n";
+        cout << "7. Peek lowest priority\n";
+        cout << "8. Peek oldest item\n";
+        cout << "9. Peek newest item\n";
+        cout << "10. Show all items\n";
+        cout << "0. Exit\n";
+        cout << "Choose: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            string item;
+            int priority;
+
+            cout << "Enter item: ";
+            cin >> item;
+
+            cout << "Enter priority: ";
+            cin >> priority;
+
+            queue.enqueue(item, priority);
+            cout << "Item added.\n";
+        }
+        else if (choice == 2) {
+            BiDirectionalPriorityQueue::Entry result;
+            if (queue.dequeue(AccessMode::Highest, result)) {
+                cout << "Removed highest priority item:\n";
+                printEntry(result);
+            }
+            else {
+                cout << "Queue is empty.\n";
+            }
+        }
+        else if (choice == 3) {
+            BiDirectionalPriorityQueue::Entry result;
+            if (queue.dequeue(AccessMode::Lowest, result)) {
+                cout << "Removed lowest priority item:\n";
+                printEntry(result);
+            }
+            else {
+                cout << "Queue is empty.\n";
+            }
+        }
+        else if (choice == 4) {
+            BiDirectionalPriorityQueue::Entry result;
+            if (queue.dequeue(AccessMode::Oldest, result)) {
+                cout << "Removed oldest item:\n";
+                printEntry(result);
+            }
+            else {
+                cout << "Queue is empty.\n";
+            }
+        }
+        else if (choice == 5) {
+            BiDirectionalPriorityQueue::Entry result;
+            if (queue.dequeue(AccessMode::Newest, result)) {
+                cout << "Removed newest item:\n";
+                printEntry(result);
+            }
+            else {
+                cout << "Queue is empty.\n";
+            }
+        }
+        else if (choice == 6) {
+            BiDirectionalPriorityQueue::Entry result;
+            if (queue.peek(AccessMode::Highest, result)) {
+                cout << "Highest priority item:\n";
+                printEntry(result);
+            }
+            else {
+                cout << "Queue is empty.\n";
+            }
+        }
+        else if (choice == 7) {
+            BiDirectionalPriorityQueue::Entry result;
+            if (queue.peek(AccessMode::Lowest, result)) {
+                cout << "Lowest priority item:\n";
+                printEntry(result);
+            }
+            else {
+                cout << "Queue is empty.\n";
+            }
+        }
+        else if (choice == 8) {
+            BiDirectionalPriorityQueue::Entry result;
+            if (queue.peek(AccessMode::Oldest, result)) {
+                cout << "Oldest item:\n";
+                printEntry(result);
+            }
+            else {
+                cout << "Queue is empty.\n";
+            }
+        }
+        else if (choice == 9) {
+            BiDirectionalPriorityQueue::Entry result;
+            if (queue.peek(AccessMode::Newest, result)) {
+                cout << "Newest item:\n";
+                printEntry(result);
+            }
+            else {
+                cout << "Queue is empty.\n";
+            }
+        }
+        else if (choice == 10) {
+            queue.printAll();
+        }
+        else if (choice == 0) {
+            cout << "Program finished.\n";
+        }
+        else {
+            cout << "Invalid choice.\n";
+        }
+
+    } while (choice != 0);
+
+    return 0;
+}
 
    
